0d165c3d7a2db777e02ca82885ed8dab4fc3d96b
Rishabh <rishabhdhiman321@gmail.com>
2019-08-19 16:27:50 +0530

    checking commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './home/frontpage/slider/slider.component';
+import { FrontpageComponent } from './home/frontpage/frontpage.component';
+import { AboutComponent } from './home/about/about.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { ShopComponent } from './home/shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SliderComponent,
+    FrontpageComponent,
+    AboutComponent,
+    ContactComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule
