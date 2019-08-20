b8fb306ea45cbedc0cda0bdbe5a59da8b7aac97d
Rishabh <rishabhdhiman321@gmail.com>
2019-08-20 19:29:58 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FrontpageComponent } from './home/frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule
