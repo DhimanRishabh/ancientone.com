f58fdce3cfa62f0cc9138259f6fd4f75808d96c3
Rishabh <rishabhdhiman321@gmail.com>
2019-09-12 18:14:34 +0530

    identity start

--- src/app/app-routes/app-routes.module.ts
+++ src/app/app-routes/app-routes.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from '../app.component';
 import {HomeComponent} from '../home/home.component';
 import {ShopComponent} from '../home/shop/shop.component';
 import {FrontpageComponent} from '../home/frontpage/frontpage.component';
-import {JoinUsComponent} from "../join-us/join-us.component";
+import {JoinUsComponent} from '../join-us/join-us.component';
 
-const appRoutes:Routes=[
-  {path:'identity',component:JoinUsComponent},
-  {path:'',component:HomeComponent,children:[
-      {path:':id',component:FrontpageComponent},
-      {path:':id1/:id2/:id3',component:ShopComponent}
-    ]}
+const appRoutes: Routes = [
+  {path: 'identity', component: JoinUsComponent},
+  {
+    path: '', component: HomeComponent, children: [
+      {path: ':id', component: FrontpageComponent},
+      {path: ':id1/:id2/:id3', component: ShopComponent}
+    ]
+  }
 
 ]
 
@@ -22,8 +24,9 @@ const appRoutes:Routes=[
     CommonModule,
     RouterModule.forRoot(appRoutes)
   ]
-  ,exports:[
+  , exports: [
     RouterModule
   ]
 })
-export class AppRoutesModule { }
+export class AppRoutesModule {
+}
--- src/app/join-us/join-us.component.ts
+++ src/app/join-us/join-us.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./join-us.component.css']
 })
 export class JoinUsComponent implements OnInit {
-
+  showSignUp:boolean=false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showSignUpFn(value: boolean) {
+    this.showSignUp=value;
+  }
 }
